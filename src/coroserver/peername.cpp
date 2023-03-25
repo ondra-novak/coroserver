24a171f6e7ffabab69d5deb7c067ab7c6be00577
Ondřej Novák <ondra-novak@email.cz>
2023-03-25 13:39:53 +0100

    peer name

---
+++ src/coroserver/peername.cpp
@@ -0,0 +1,328 @@
+#include "peername.h"
+
+#include <arpa/inet.h>
+#include <stdexcept>
+#include <unistd.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/un.h>
+#include <arpa/inet.h>
+#include <cstring>
+#include <system_error>
+
+namespace coroserver {
+
+const unsigned int PeerName::max_sockaddr_size = sizeof(sockaddr_storage);
+const std::string_view PeerName::unix_prefix = "unix:";
+
+
+
+struct PeerName::ToStringHlp {
+    std::string operator()(const IPv4 &addr) const {
+        int a = (addr.addr >> 24) & 0xFF;
+        int b = (addr.addr >> 16) & 0xFF;
+        int c = (addr.addr >> 8) & 0xFF;
+        int d = (addr.addr /*>> 0*/) & 0xFF;
+        int port = htons(addr.port);
+        char buff[24];
+        snprintf(buff,24,"%d.%d.%d.%d:%d",a,b,c,d,port);
+        return buff;
+    }
+    std::string operator()(const IPv6 &addr) const {
+        char buff[50];
+        snprintf(buff, 50, "[%04x:%04x:%04x:%04x:%04x:%04x:%04x:%04x]:%d",
+                htons(addr.addr[0]),
+                htons(addr.addr[1]),
+                htons(addr.addr[2]),
+                htons(addr.addr[3]),
+                htons(addr.addr[4]),
+                htons(addr.addr[5]),
+                htons(addr.addr[6]),
+                htons(addr.addr[7]),
+                htons(addr.port));
+        return buff;
+    }
+    std::string operator()(const Unix &addr) const {
+        return std::string(unix_prefix).append(addr.path);
+    }
+    std::string operator()(const Error &addr) const {
+        try {
+            std::rethrow_exception(addr.e);
+        } catch (std::exception &e) {
+            return std::string("Error: ").append(e.what());
+        }
+
+    }
+    std::string operator()(const std::monostate &) const {
+        return "<n/a>";
+
+    }
+};
+
+std::string PeerName::to_string() const {
+    return visit(ToStringHlp());
+}
+
+std::vector<PeerName> PeerName::lookup(std::string_view name, std::string_view def_port) {
+    std::vector<PeerName> list;
+    while (!name.empty()) {
+        std::string_view item;
+        auto sep = name.find(' ');
+        if (sep == name.npos) {
+            item = name;
+            name = {};
+        } else {
+            item = name.substr(0, sep);
+            name = name.substr(sep+1);
+        }
+        if (item.empty()) continue;
+
+        try {
+
+            if (item.compare(0,unix_prefix.size(), unix_prefix) == 0) {
+                    resolve_local(item.substr(unix_prefix.size()),list);
+            }
+
+            std::string_view host;
+            std::string_view port;
+
+            if (item[0] == '[') {//ipv6
+                auto spos = item.find(']');
+                if (spos != item.npos) {
+                    host = item.substr(1,spos-1);
+                    spos = item.find(':', spos);
+                    if (spos == item.npos) {
+                        port = def_port;
+                    } else {
+                        port = item.substr(spos+1);
+                    }
+                }
+            }
+            if (host.empty()) {
+                auto spos = item.rfind(':');
+                if (spos == item.npos) {
+                    port = def_port;
+                    host = item;
+                } else {
+                    port = item.substr(spos+1);
+                    host = item.substr(0,spos);
+                }
+            }
+
+            if (port.empty()) {
+                list.push_back(Error{std::make_exception_ptr(PortIsRequiredException())});
+            } else {
+                nslookup(std::string(host), std::string(port),list);
+            }
+        } catch (...) {
+            list.push_back(Error{std::current_exception()});
+        }
+    }
+    if (list.empty()) {
+        throw std::invalid_argument("PeerName::lookup(\"\") can't be resolved");
+    }
+    for (const auto &x: list) if (x.valid()) return list;
+    const Error *e = list[0].get_error() ;
+    if (e) std::rethrow_exception(e->e);
+    throw std::runtime_error("Not valid address returned");
+
+}
+
+void PeerName::nslookup(std::string &&host, std::string &&port, std::vector<PeerName> &list) {
+    addrinfo hint = {};
+    addrinfo *result;
+
+    const char *hostptr = host.c_str();
+
+    hint.ai_family = AF_UNSPEC;
+    hint.ai_protocol = IPPROTO_TCP;
+    if (host == "localhost" || host == "0") {
+        hostptr = nullptr;
+    }
+    else if (host == "*" || host.empty())  {
+        hostptr = nullptr;
+        hint.ai_flags = AI_PASSIVE;
+    }
+    hint.ai_flags |= AI_ADDRCONFIG;
+
+
+    int err = getaddrinfo(hostptr, port.c_str(),&hint,&result);
+
+    if (err) {
+        throw LookupException(err);
+    }
+
+    addrinfo *iter = result;
+    while (iter != nullptr) {
+        PeerName p = PeerName::from_sockaddr(iter->ai_addr);
+        if (p.valid()) list.push_back(std::move(p));
+        iter = iter->ai_next;
+    }
+
+    freeaddrinfo(result);
+}
+
+
+
+unsigned int PeerName::to_sockaddr(void *sockaddr, unsigned int sockaddr_len) const {
+    std::memset(sockaddr, 0, sockaddr_len);
+    return visit([&](const auto &item) -> unsigned int {
+        using Type = std::decay_t<decltype(item)>;
+        if constexpr(std::is_same_v<Type, IPv4>) {
+            constexpr unsigned int sz = sizeof(sockaddr_in);
+            if (sockaddr_len < sz) return 0;
+            auto *sin = reinterpret_cast<sockaddr_in *>(sockaddr);
+            sin->sin_family = AF_INET;
+            sin->sin_addr.s_addr = item.addr;
+            sin->sin_port = item.port;
+            return sz;
+        } else if constexpr(std::is_same_v<Type, IPv6>) {
+            constexpr unsigned int sz = sizeof(sockaddr_in6);
+            if (sockaddr_len < sz) return 0;
+            auto *saddr = reinterpret_cast<sockaddr_in6 *>(sockaddr);
+            saddr->sin6_family = AF_INET6;
+            std::copy(std::begin(item.addr), std::end(item.addr), std::begin(saddr->sin6_addr.__in6_u.__u6_addr16));
+            saddr->sin6_flowinfo = item.flow_info;
+            saddr->sin6_scope_id = item.scope_id;
+            return sz;
+        } else if constexpr(std::is_same_v<Type, Unix>) {
+            constexpr unsigned int sz = sizeof(sockaddr_un);
+            if (sockaddr_len < sz) return 0;
+            auto *saddr = reinterpret_cast<sockaddr_un *>(sockaddr);
+            saddr->sun_family = AF_UNIX;
+            std::string p = item.path.string();
+            if (p.size() >= sizeof(saddr->sun_path)) throw std::runtime_error("Path is too long");
+            std::strncpy(saddr->sun_path, p.c_str(),sizeof(saddr->sun_path));
+            return sz;
+        } else if constexpr(std::is_same_v<Type, Error>) {
+            std::rethrow_exception(item.e);
+        } else {
+            return 0;
+        }
+    });
+}
+
+PeerName PeerName::from_sockaddr(const void *s) {
+    auto *saddr = reinterpret_cast<const sockaddr *>(s);
+    switch (saddr->sa_family) {
+        case AF_INET: {
+            auto *addr = reinterpret_cast<const sockaddr_in *>(s);
+            return IPv4{addr->sin_addr.s_addr, addr->sin_port};
+        }
+        case AF_INET6: {
+            IPv6 r;
+            auto *addr = reinterpret_cast<const sockaddr_in6 *>(s);
+            std::copy(std::begin(addr->sin6_addr.__in6_u.__u6_addr16),
+                      std::end(addr->sin6_addr.__in6_u.__u6_addr16),
+                      std::begin(r.addr));
+            r.flow_info = addr->sin6_flowinfo;
+            r.scope_id = addr->sin6_scope_id;
+            r.port = addr->sin6_port;
+            return r;
+        }
+        case AF_UNIX: {
+            auto *addr = reinterpret_cast<const sockaddr_un *>(s);
+            return Unix{addr->sun_path, 0};
+        }
+        default:
+            throw std::invalid_argument("Unsupported address type");
+
+    }
+}
+
+struct PeerName::HasherHelper {
+    std::size_t operator()(const IPv4 &addr) const {
+       return (addr.addr<<16)+addr.port;
+    }
+    std::size_t operator()(const IPv6 &addr) const {
+        std::string_view b(reinterpret_cast<const char *>(&addr),sizeof(addr));
+        return std::hash<std::string_view>()(b);
+    }
+    std::size_t operator()(const Unix &addr) const {
+        std::hash<std::string_view> h;
+        return h(addr.path.string());
+    }
+    std::size_t operator()(const Error &) const {
+        return 888;
+    }
+    std::size_t operator()(const std::monostate &) const {
+        return -7777;
+
+    }
+
+};
+
+
+std::size_t PeerName::hash() const {
+    return visit(HasherHelper());
+}
+
+bool PeerName::equal_to(const PeerName &other) const {
+    return _content == other._content;
+}
+
+void PeerName::resolve_local(std::string_view addr, std::vector<PeerName> &list) {
+    auto splt = addr.rfind(':');
+    int permission = 0;
+    if (splt != addr.npos) {
+        auto iter = addr.begin() + splt+1;
+        auto end = addr.end();
+        while (iter != end) {
+            auto c = *iter;
+            ++iter;
+            switch (c) {
+                case '0': permission = permission * 8;break;
+                case '1': permission = permission * 8 + 1;break;
+                case '2': permission = permission * 8 + 2;break;
+                case '3': permission = permission * 8 + 3;break;
+                case '4': permission = permission * 8 + 4;break;
+                case '5': permission = permission * 8 + 5;break;
+                case '6': permission = permission * 8 + 6;break;
+                case '7': permission = permission * 8 + 7;break;
+                case 'u': permission = permission | S_IRUSR | S_IWUSR; break;
+                case 'g': permission = permission | S_IRGRP | S_IWGRP; break;
+                case 'o': permission = permission | S_IROTH | S_IWOTH; break;
+                default:
+                    splt = addr.length();
+                    iter = end;
+                    break;
+            }
+        }
+        addr = addr.substr(0, splt);
+    }
+    std::filesystem::path p(addr);
+    p = std::filesystem::weakly_canonical(p);
+    if (p.string().size() >= sizeof(sockaddr_un::sun_path)) {
+        throw std::invalid_argument("Path to unix socket exceeded maximum 107 characters. Longer paths are not supported.");
+    }
+    list.push_back(PeerName(PeerName::Unix{std::move(p), permission}));
+
+}
+
+
+const char* PeerName::PortIsRequiredException::what() const noexcept {
+    return "Port is required. Use <address>:<port> as peer name";
+}
+
+PeerName::LookupException::LookupException(int err):_err(err) {
+
+}
+
+const char* PeerName::LookupException::what() const noexcept {
+    return gai_strerror(_err);
+}
+
+bool PeerName::IPv6::operator ==(const IPv6 &x) const {
+    if (!std::equal(std::begin(addr), std::end(addr), std::begin(x.addr))) {
+        return false;
+    }
+    return port == x.port;
+}
+
+bool PeerName::Unix::operator ==(const Unix &x) const {
+    return path == x.path;
+}
+
+}
---
+++ src/coroserver/peername.h
@@ -0,0 +1,231 @@
+/*
+ * peername.h
+ *
+ *  Created on: 25. 3. 2023
+ *      Author: ondra
+ */
+
+#ifndef SRC_COROSERVER_PEERNAME_H_
+#define SRC_COROSERVER_PEERNAME_H_
+#include <cstdint>
+#include <exception>
+#include <filesystem>
+#include <string>
+#include <variant>
+#include <vector>
+
+
+struct sockaddr;
+
+namespace coroserver {
+
+
+///Contans network address which can be used to listen or connect oder peer
+class PeerName {
+public:
+    static const unsigned int max_sockaddr_size;
+    static const std::string_view unix_prefix;
+
+
+    struct IPv4 {
+        std::uint32_t addr;
+        std::uint16_t port;
+        bool operator == (const IPv4 &x) const {
+            return addr == x.addr && port == x.port;
+        }
+    };
+
+    struct IPv6 {
+        std::uint16_t addr[8];
+        std::uint16_t port;
+        std::uint32_t flow_info;
+        std::uint32_t scope_id;
+        bool operator == (const IPv6 &x) const;
+    };
+
+    struct Unix {
+        std::filesystem::path path;
+        int perms;
+        bool operator == (const Unix &x) const;
+    };
+
+    struct Error {
+        std::exception_ptr e;
+        bool operator == (const Error &) const {
+            return false;
+        }
+    };
+
+    ///Construct default peer name, which has no value
+    /** Content of variable doesn't refer any peer, and can't be used */
+    PeerName() = default;
+    ///Construct IPv4 address
+    PeerName(IPv4 addr):_content(std::move(addr)) {}
+    ///Construct IPv6 address
+    PeerName(IPv6 addr):_content(std::move(addr)) {}
+    ///Construct Unix socket address
+    PeerName(Unix addr):_content(std::move(addr)) {}
+    ///Construct error address
+    PeerName(Error addr):_content(std::move(addr)) {}
+
+    ///Convert address to string
+    std::string to_string() const;
+    ///Perform address lookup
+    /**
+     * @param name requested peer name. You can request multiple peers, where each peer is separated by space.
+     * @param def_port default port, when there is no port specification. If this parameter is not set, all peers must have a port
+     * @return list of found peers. If no peer is found, exception is thrown, so the result contains always at least one peer
+     */
+    static std::vector<PeerName> lookup(std::string_view name, std::string_view def_port = {});
+
+
+    ///constructs sockaddr from peer, to perform low-level network access
+    /**
+     * @param sockaddr pointer to buffer where sockaddr will be constructed
+     * @param sockaddr_len size of the buffer
+     * @return returns count of bytes written to the buffer, return 0, if operation cannot be performed
+     */
+    unsigned int to_sockaddr(void *sockaddr, unsigned int sockaddr_len) const;
+
+    ///constructs PeerName from sockaddr
+    /**
+     * @param sockaddr pointer to sockaddr
+     * @return constructed objecet
+     */
+    static PeerName from_sockaddr(const void *sockaddr);
+
+    ///Visit the content
+    /**
+     * @param fn function, it can receive any argument depend on type of address
+     * @return return value of function
+     */
+    template<typename Fn>
+    auto visit(Fn &&fn) const {
+        return std::visit(std::forward<Fn>(fn), _content);
+    }
+
+    ///Calls function with sockaddr * and socklen passed as arguments
+    /**
+     * @param fn function to call (sockaddr *, socklen)
+     * @return return value of function
+     */
+    template<typename Fn>
+    auto use_sockaddr(Fn &&fn) {
+        unsigned int sz = max_sockaddr_size;
+        void *buff = alloca(max_sockaddr_size);
+        sz = to_sockaddr(buff, sz);
+        return fn(reinterpret_cast<const sockaddr *>(buff), sz);
+    }
+
+    ///Captures sockaddr and receives PeerName
+    /**
+     * @param fn function to be called, it receives pointer to sockaddr and size of the reserved space.
+     * Function must returns count of written bytes, or zero if error
+     * @return PeerName if nonzero bytes has been written, or empty PeerName if error.
+     */
+    template<typename Fn>
+    static PeerName capture_sockaddr(Fn &&fn) {
+        unsigned int sz = max_sockaddr_size;
+        void *buff = alloca(max_sockaddr_size);
+        unsigned int capsz = fn(buff, sz);
+        if (capsz == 0) return PeerName();
+        else return from_sockaddr(buff);
+    }
+
+    ///Determines, whether peer name is valid
+    /**
+     * @retval true is valid
+     * @retval false variable doesn't contains value, or contains error
+     */
+    bool valid() const {
+        return !std::holds_alternative<std::monostate>(_content) &&
+                !std::holds_alternative<Error>(_content);
+    }
+
+    ///Retrieve IPv4 informations
+    /**
+     * @return pointer to IPv4 struct, or nullptr if content is not IPv4
+     */
+    const IPv4 *get_ipv4() const {
+        if (std::holds_alternative<IPv4>(_content)) return &std::get<IPv4>(_content);
+        else return nullptr;
+    }
+    ///Retrieve IPv6 informations
+    /**
+     * @return pointer to IPv6 struct, or nullptr if content is not IPv6
+     */
+    const IPv6 *get_ipv6() const {
+        if (std::holds_alternative<IPv6>(_content)) return &std::get<IPv6>(_content);
+        else return nullptr;
+    }
+    ///Retrieve UnixSocket informations
+    /**
+     * @return pointer to UNix struct, or nullptr if content is not Unix
+     */
+    const Unix *get_unix() const {
+        if (std::holds_alternative<Unix>(_content)) return &std::get<Unix>(_content);
+        else return nullptr;
+    }
+    ///Retrieve Error informations
+    const Error *get_error() const {
+        if (std::holds_alternative<Unix>(_content)) return &std::get<Error>(_content);
+        else return nullptr;
+    }
+
+    ///Type of stored information
+    enum class Type {
+        invalid = 0,
+        ipv4 = 1,
+        ipv6 = 2,
+        unix_socket = 3,
+        error = 4
+    };
+
+    ///Get type of peer name
+    Type getType() {return static_cast<Type>(_content.index());}
+
+    ///Retrieve hash for hashing for unordered map
+    std::size_t hash() const;
+    ///Compare with other address
+    bool equal_to(const PeerName &other) const;
+
+    ///Exception
+    class PortIsRequiredException: public std::exception {
+    public:
+        virtual const char *what() const noexcept override;
+    };
+
+    ///Exception
+    class LookupException: public std::exception {
+    public:
+        LookupException(int err);
+        const char *what() const noexcept override;
+        int code() const {return _err;}
+    protected:
+        int _err;
+
+    };
+
+    bool operator==(const PeerName &x) const {
+        return equal_to(x);
+    }
+protected:
+
+    std::variant<std::monostate,IPv4, IPv6, Unix, Error> _content;
+
+    static void nslookup(std::string &&host, std::string &&port, std::vector<PeerName> &list);
+    static void resolve_local(std::string_view path, std::vector<PeerName> &list);
+
+    struct ToStringHlp;
+    struct HasherHelper;
+};
+
+
+
+
+}
+
+
+
+
+#endif /* SRC_COROSERVER_PEERNAME_H_ */
