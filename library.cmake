0f604aef2e02aa69bf6fba6486ce9b499be28de6
Ondřej Novák <ondra-novak@email.cz>
2023-05-13 21:51:14 +0200

    fix lib

--- library.cmake
+++ library.cmake
@@ -1,2 +1,3 @@
 include_directories(AFTER ${CMAKE_CURRENT_LIST_DIR}/src)
 add_subdirectory (${CMAKE_CURRENT_LIST_DIR}/src/coroserver EXCLUDE_FROM_ALL)
+add_subdirectory (${CMAKE_CURRENT_LIST_DIR}/version EXCLUDE_FROM_ALL)
