f92a173b2e68928bb92dda862c784970a0ad0c4f
Ondřej Novák <novak@cooberfund.com>
2025-02-24 11:15:31 +0100

    prepare tests

---
+++ src/tests/tcp_test.cpp
@@ -0,0 +1,50 @@
+#include "check.h"
+
+#include <coroserver/network.h>
+#include <coroserver/stream.h>
+#include <coroserver/character_io.h>
+
+
+coro::async<void> write_task(coroserver::Context &ctx, std::string port) {
+    coroserver::Stream stream = co_await ctx.connect(PeerName::lookup("localhost", port));
+    coroserver::CharacterWriter<coroserver::Stream> wr(stream);
+    for (int i = 0; i < 655360; i++) {
+        co_await wr(static_cast<char>(i & 0xFF));
+    }
+    co_await wr.flush();
+}
+
+coro::async<void> read_task(coroserver::Stream s) {
+    int x = 0;
+    int cnt = 0;
+    coroserver::CharacterReader<coroserver::Stream> rd(s);
+    int c = co_await rd;
+    while (c != -1) {
+        CHECK_EQUAL((x & 0xFF) , c);
+        c = co_await rd;
+        ++x;
+        ++cnt;
+    }
+    CHECK_EQUAL(cnt, 655360);
+}
+
+coro::async<void> server_task(coro::deferred_future<coroserver::Stream> &&f) {
+
+    coroserver::Stream s = co_await f;
+    co_await read_task(s);
+
+}
+
+int main() {
+
+    coroserver::Context ctx;
+
+    auto addr = PeerName::lookup("127.0.0.1","*");
+    auto listener = ctx.accept(addr);
+
+    write_task(ctx, addr[0].get_port()).detach();
+
+    ctx.start(server_task(listener()));
+
+
+}
